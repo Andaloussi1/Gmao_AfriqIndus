5c6ae1561039bf4b9ea68d2d07b12a6b7bf5f266
Anas <firarianas@gmail.com>
2022-08-11 13:02:34 +0100

    Added Formkit

--- resources/js/Pages/Articles/Index.vue
+++ resources/js/Pages/Articles/Index.vue
@@ -46,10 +46,14 @@ const destroy=(id)=>{
                         class="border-t"
                     >
                         <template v-if="column.key === 'actions'">
-                            <Link :href="route('articles.edit',article.id)" class="flex-1 bg-green-500 hover:bg-green-600 text-white rounded">Modifier
+                            <Link :href="route('articles.edit',article.id)" class="flex-1 rounded">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                </svg>
                             </Link>
                             <button @click="destroy(article.id)" class="flex-1 bg-red-600 hover:bg-red-900 text-white rounded">Supprimer
                             </button>
+
                         </template>
                         <Link v-else="" :href="route('articles.show',article.id)" class="px-6 py-4 flex items-center">
                             {{ article[column.key] }}
